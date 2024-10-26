0ec87b55038093ffacf8260f0800ee403ab51c83
briskgaurav <briskgaurav@gmail.com>
2024-10-26 19:19:39 +0530

    PickNest A Three JS Project

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -34,7 +34,7 @@ useGSAP(() => {
   return (
     <div className='lg:w-1/2 lg:h-full w-full  h-2/5 flex items-center justify-center'>
       <div className='flex items-start flex-col justify-start w-full px-8  lg:px-0 lg:w-2/3 gap-2 lg:gap-4'>
-        <h1 className='lg:text-right text-center w-full font-bold overflow-hidden text-4xl lg:text-9xl'>
+        <h1 className='lg:text-right text-center w-full font-bold overflow-hidden text-4xl lg:text-8xl'>
           <p className='text uppercase'>{data.heading}</p>
         </h1>
         
@@ -47,8 +47,8 @@ useGSAP(() => {
         </p>
 
         <div className='lg:text-xl text-md  lg:font-medium lg:text-right text-center overflow-hidden w-full'>
-          <button className='button text py-2 px-5  lg:px-10 lg:py-4 rounded-xl mt-4 lg:mt-10'>
-            Shop Now!
+          <button className='button text py-2 px-5 lg:font-medium  lg:px-10 lg:py-4 rounded-xl mt-4 lg:mt-10'>
+           SHOP NOW
           </button >
         </div>
       </div>
